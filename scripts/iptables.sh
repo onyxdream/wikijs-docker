b8a54ffad67bf96dc2b326beb8d08807702515e2
onyxdream <dragging.commute349@passmail.com>
2026-03-31 23:11:26 +0200

    Base environment, docker compose main layout (psql, wikijs, nginx), iptables script for host and ldap integration (in process).

---
+++ scripts/iptables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LAN_NETWORK="192.168.70.0/23"
+
+iptables -F
+iptables -X
+
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A INPUT -p tcp --dport 22 -s $LAN_NETWORK -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -s $LAN_NETWORK -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -s $LAN_NETWORK -j ACCEPT
+
+iptables -A INPUT -p icmp -j ACCEPT
